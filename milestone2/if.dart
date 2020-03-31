febff966a74103632e8ff4d630e95609729c8272
honestus-codice <laser@email.arizona.edu>
2020-03-31 13:12:02 -0700

    adding if.dart

---
+++ milestone2/if.dart
@@ -0,0 +1,8 @@
+//an example of Dart's if statement syntax
+import 'dart:io';
+main(){
+  String str = "Hello world";
+  for (int i = 0; i< str.length; i++){
+    stdout.write(str[i]); //notice you can index into a string in dart
+  } 
+}
