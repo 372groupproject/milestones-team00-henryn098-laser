b91475d8dcd7842dd1509333282651f70129c48c
honestus-codice <laser@email.arizona.edu>
2020-04-21 20:16:28 -0700

    Update p4_interface.dart

--- milestone4/p4_interface.dart
+++ milestone4/p4_interface.dart
@@ -1,7 +1,7 @@
 import 'p4_classes2.dart';
 /*in dart, classes are interfaces,
-the only difference is that a subclass which 
-provides concrete implementation of all functions*/
+the only difference is that a subclass must
+provide concrete implementation of all functions*/
 
 class Display{
   void displayGrades(){}
